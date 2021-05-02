ffcff37ec7db0d26a25de6144548e0d8ad135ecc
J4RD0 <jarodholgado@gmail.com>
2021-05-02 14:35:17 -0400

    changed to maven project for GSON and any other dependencies deemed necessary

---
+++ src/main/java/CalendarReader.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import com.google.gson.*;
+
+
+public class CalendarReader{
+    public static void main(String[] args) throws FileNotFoundException{
+        //test code for Date structure
+        TimeSlot new_timeslot = new TimeSlot(1400, 1500);
+        TimeSlot new_timeslot2 = new TimeSlot(1600, 1700);
+        Date new_date = new Date(4, 28);
+        new_date.addTimeSlot(new_timeslot);
+        new_date.addTimeSlot(new_timeslot2);
+        System.out.println(new_date);
+    }    
+}
