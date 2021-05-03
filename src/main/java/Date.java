2902a9afd30c46a714ce63140be70915b2068676
J4RD0 <jarodholgado@gmail.com>
2021-05-03 16:51:40 -0400

    json reader completed, however, file could improve aesthetically and currently does not work if there are line gaps in between date entries

--- src/main/java/Date.java
+++ src/main/java/Date.java
@@ -1,14 +1,16 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Date{
+public class Date implements Comparable<Date>{
     private int month;
     private int day;
+    private int year;
     List<TimeSlot> time_slots = new ArrayList<>();
 
-    Date(int month, int day){
+    Date(int month, int day, int year){
         this.month = month;
         this.day = day;
+        this.year = year;
     }
 
     public void addTimeSlot(TimeSlot new_slot){
@@ -16,8 +18,21 @@ public class Date{
     }
 
     @Override
+    public int compareTo(Date o){
+        if(this.year != o.year){
+            return this.year - o.year;
+        }
+        else if(this.month != o.month) {
+            return this.month - o.month;
+        }
+        else {
+            return this.day - o.day;
+        }
+    }
+
+    @Override
     public String toString(){
-        return month + "/" + day + "\n" + time_slots.toString();
+        return month + "/" + day + "/" + year + "\n" + time_slots.toString();
     }
 
 } 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,63 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.*;
 import com.google.gson.*;
 
 
 public class Main{
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws IOException, ClassNotFoundException{
         //test code for Date structure
         TimeSlot new_timeslot = new TimeSlot(1400, 1500);
         TimeSlot new_timeslot2 = new TimeSlot(1600, 1700);
-        Date new_date = new Date(4, 28);
+        
+        Date new_date = new Date(4, 28, 2021);
+        Date new_date2 = new Date(4, 29, 2021);
+        
+        new_date2.addTimeSlot(new_timeslot);
+        new_date2.addTimeSlot(new_timeslot2);
+        
         new_date.addTimeSlot(new_timeslot);
         new_date.addTimeSlot(new_timeslot2);
-        System.out.println(new_date);
+         
+        List<Date> calendar_output = new ArrayList<>(); 
+        calendar_output.add(new_date2);
+        calendar_output.add(new_date);
+        Collections.sort(calendar_output);
+
+        for(Date date : calendar_output){
+            System.out.println(date);
+        }
+
+        //write list of dates to json file
+        final Gson gson = new Gson();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("json_objects.json"));
+        for (Date date : calendar_output){
+            gson.toJson(date, writer);
+            writer.newLine();
+        }        
+        writer.close();
+
+        //Read File back
+        List<String> json_input = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader("calendar.json"));
+        
+        String line;
+        while ((line = reader.readLine()) != null) {
+          json_input.add(line);
+        }
+
+        reader.close();
+
+        List<Date> calendar_input = new ArrayList<>();
+
+        for(String date_json : json_input){
+            calendar_input.add(gson.fromJson(date_json, Date.class));
+        }
+
+        Collections.sort(calendar_input);
+
+        for(Date date : calendar_input){
+            System.out.println(date);
+        }
+
     }    
 }
