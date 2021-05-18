427072901b82e9e1c01b1ec4f0ef4904635af53a
cgiordano16 <cgiorda1@stevens.edu>
2021-05-18 18:54:20 -0400

    Remove commented code

--- src/main/java/web/controller/CalendarDate.java
+++ src/main/java/web/controller/CalendarDate.java
@@ -51,37 +51,8 @@ public class CalendarDate {
 			displayDate = " ";
 		} else {
 			displayDate = String.format("%s", calendarDate.getDayOfMonth());
-			// createAppointments();
 		}
 	}
-	
-	// public static String createAppointments(AppointmentDatabase appointmentDb, LocalDate calendarDate, int startHour, int startMinute, int endingHour, int endingMinute) {
-	// 	LocalTime startingTime = LocalTime.of(startHour, startMinute);
-	// 	LocalDateTime startingAppt = LocalDateTime.of(calendarDate, startingTime);
-		
-	// 	// Skip Sat and Sun
-	// 	if (startingAppt.getDayOfWeek() == DayOfWeek.SUNDAY || startingAppt.getDayOfWeek() == DayOfWeek.SATURDAY) {
-	// 		return "";
-	// 	}
-		
-	// 	LocalTime endingTime = LocalTime.of(endingHour, endingMinute);
-	// 	LocalDateTime endingAppt = LocalDateTime.of(calendarDate, endingTime);
-		
-	// 	LocalDateTime apptTimeSlot = startingAppt;
-	// 	Appointment appt = appointmentDb.find(apptTimeSlot);
-	// 	LocalDateTime ldt = appt.getStartingTime();
-	// 	Map<LocalDateTime, Appointment> apptMap = ldt.getAppointmentMap();
-	// 	while(apptTimeSlot.isBefore(endingAppt)) {
-	// 		Appointment appt = new Appointment("<Available Slot>", apptTimeSlot.getYear(), apptTimeSlot.getMonthValue(), apptTimeSlot.getDayOfMonth(), apptTimeSlot.getHour(), apptTimeSlot.getMinute());
-	// 		String errorString = appointmentDb.update(apptTimeSlot, appt);
-	// 		if (!errorString.isEmpty()) {
-	// 			return errorString;
-	// 		}
-	// 		apptMap.put(apptTimeSlot, appt);
-	// 		apptTimeSlot = apptTimeSlot.plusMinutes(CalendarDate.lengthOfAppointment);
-	// 		return "";
-	// 	}
-	// }
 
 	public void addAppointment(Appointment appointment) {
 		if (calendarDate != null) {
